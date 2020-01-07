a214c9e8479e810b11c638665cbd458d16cc86b5
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-01-06 23:56:40 -0300

    utilizando BehaviorSubject

--- lib/widgets/videotile.dart
+++ lib/widgets/videotile.dart
@@ -50,7 +50,6 @@ class VideoTile extends StatelessWidget {
               ),
               StreamBuilder<Map<String, Video>>(
                 stream: bloc.outFav,
-                initialData: {},
                 builder: (context, snapshot) {
                   if (snapshot.hasData)
                     return IconButton(
