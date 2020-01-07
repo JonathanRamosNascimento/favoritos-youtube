63cbb9ccb53713447598fee44bece3650cc1f097
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-01-06 21:59:45 -0300

    Criando lista infinita, carregando mais após chegar ao ultimo item da lista

--- lib/blocs/videos_bloc.dart
+++ lib/blocs/videos_bloc.dart
@@ -5,25 +5,32 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:fluttertube/models/video.dart';
 
 class VideosBloc extends BlocBase {
-
   Api api;
 
   List<Video> videos;
 
-  final StreamController<List<Video>> _videosController = StreamController<List<Video>>();
+  final StreamController<List<Video>> _videosController =
+      StreamController<List<Video>>();
+
   Stream get outVideos => _videosController.stream;
 
   final StreamController<String> _searchController = StreamController<String>();
+
   Sink get inSearch => _searchController.sink;
 
-  VideosBloc(){
+  VideosBloc() {
     api = Api();
 
     _searchController.stream.listen(_search);
   }
 
   void _search(String search) async {
-    videos = await api.search(search);
+    if (search != null) {
+      _videosController.sink.add([]);
+      videos = await api.search(search);
+    } else {
+      videos += await api.nextPage();
+    }
     _videosController.sink.add(videos);
   }
 
